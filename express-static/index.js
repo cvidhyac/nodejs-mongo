467f50e4db779f59eadc4943e216e896393cd9d8
Vidhya Chidambaram <vidhya.chidambaram@gmail.com>
2020-11-16 15:32:22 -0500

    update example for express-edge

--- express-static/index.js
+++ express-static/index.js
@@ -1,14 +1,31 @@
-const { response } = require('express')
 const express = require('express')
 const path = require('path')
 
 const app = express()
 app.use(express.static('public'))
+const { config, engine } = require('express-edge');
+ 
+// Automatically sets view engine and adds dot notation to app.render
+app.use(engine);
+app.set('views', `${__dirname}/views`);
 
 app.get('/', (req, res) => {
-    response.sendFile(path.resolve(__dirname, 'index.html'))
+  res.render('index')
 })
 
-app.listen(3003, () => {
-    console.log('App listening at port 3003')
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+app.get('/post')
+app.listen(3005, () => {
+    console.log('App listening at port ')
 })
