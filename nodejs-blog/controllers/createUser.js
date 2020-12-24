55819ea9c4ea3b6417fd1d73782a40ab4710b9d6
Vidhya Chidambaram <vidhya.chidambaram@gmail.com>
2020-12-23 21:10:34 -0500

    display validation error in edge

--- nodejs-blog/controllers/createUser.js
+++ nodejs-blog/controllers/createUser.js
@@ -1,3 +1,6 @@
 module.exports = (req, res) => {
-    res.render('register')
+    console.log(req.session.regErrors)
+    res.render('register', {
+        errors: req.session.regErrors
+    })
 }
--- nodejs-blog/controllers/storeUser.js
+++ nodejs-blog/controllers/storeUser.js
@@ -3,11 +3,11 @@ const user_dao = require('../database/user-dao')
 module.exports = (req, res) => {
     console.log(req.body)
     const regErrors = user_dao.create_user(req.body)
-    console.log(regErrors)
     if(regErrors) {
+        req.session.regErrors = regErrors
         return res.redirect('/auth/register')
     }
-    console.log('reached here, redirecting to homepage')
+    console.log('reached end, redirecting to homepage')
     res.redirect('/')
 }
 
